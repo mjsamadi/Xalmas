2422cdda9be66e82e5d6793d28416de3ad98b41b
Omar Dabban <105754576+omarsn1@users.noreply.github.com>
2022-11-11 01:10:40 +0300

    Add files via upload

--- Contracts/DalmasToken.sol
+++ Contracts/DalmasToken.sol
@@ -6,18 +6,20 @@ pragma solidity ^0.5.0;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Detailed.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Mintable.sol";
-
-// Create a constructor for the DalmasToken contract and have the contract inherit the libraries that you imported from OpenZeppelin.
-contract DalmasToken is ERC20, ERC20Detailed, ERC20Mintable {
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Burnable.sol";
+// Create a constructor for the DalmasCoin contract and have the contract inherit the libraries that you imported from OpenZeppelin.
+contract DalmasToken is ERC20, ERC20Detailed, ERC20Mintable, ERC20Burnable {
     constructor(
         string memory name,
         string memory symbol,
-    //    uint initial_supply
+        uint initial_supply
     )
+        ERC20Mintable()
+        ERC20Burnable()
         ERC20Detailed(name, symbol, 18)
         public
-    {
-    //  _mint(msg.sender, initial_supply);
+    {// mint(msg.sender, initial_supply);
     }
 
+    
 }
--- Contracts/DalmasTokenCrowdsale.sol
+++ Contracts/DalmasTokenCrowdsale.sol
@@ -3,19 +3,32 @@ pragma solidity ^0.5.0;
 import "./DalmasToken.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/Crowdsale.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/emission/MintedCrowdsale.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/validation/CappedCrowdsale.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/validation/TimedCrowdsale.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/distribution/RefundablePostDeliveryCrowdsale.sol";
 
-
-// Have the DalmasTokanCrowdsale contract inherit the following OpenZeppelin:
+// Have the DalmasCoinCrowdsale contract inherit the following OpenZeppelin:
 // * Crowdsale
 // * MintedCrowdsale
-contract DalmasTokenCrowdsale is Crowdsale, MintedCrowdsale { 
+contract DalmasTokenCrowdsale is Crowdsale, MintedCrowdsale, CappedCrowdsale, TimedCrowdsale, RefundableCrowdsale{ 
     constructor(
         uint256 rate,
         address payable wallet,
-        DalmasToken token
-    ) public Crowdsale(rate, wallet, token) {
+        DalmasToken token,
+        uint goal, // the crowdsale goal
+        uint open, // the crowdsale opening time
+        uint close // the crowdsale closing time
+    ) public 
+        Crowdsale(rate, wallet, token)
+        MintedCrowdsale()
+        CappedCrowdsale(goal)
+        TimedCrowdsale(open, close)
+        RefundableCrowdsale(goal)
+
+    {
         // constructor can stay empty
     }
+    
 }
 
 
@@ -27,26 +40,31 @@ contract DalmasTokenCrowdsaleDeployer {
 
     // Add the constructor.
     constructor(
-        string memory name,
+       string memory name,
         string memory symbol,
-        address payable wallet
+        address payable wallet,
+        uint goal // the crowdsale goal
+    
     ) public {
-        // Create a new instance of the DalmasToken contract.
+        // Create a new instance of the DalmasCoin contract.
         DalmasToken token = new DalmasToken (name, symbol, 0);
         
-        // Assign the token contract’s address to the `kasei_token_address` variable.
+        // Assign the token contract’s address to the `Dalmas_token_address` variable.
         Dalmas_token_address = address(token);
 
-        // Create a new instance of the `DalmasTokenCrowdsale` contract
-        DalmasTokenCrowdsale  DalmasToken_crowdsale = new DalmasTokenCrowdsale(1, wallet, token);
+        // Create a new instance of the `DalmasCoinCrowdsale` contract
+        DalmasTokenCrowdsale  DalmasToken_crowdsale =
+            new DalmasTokenCrowdsale(1, wallet, token, goal, now, now + 24 weeks);
             
-        // Aassign the `DalmasTokenCrowdsale` contract’s address to the `Dalmas_crowdsale_address` variable.
+        // Aassign the `DalmasCoinCrowdsale` contract’s address to the `Dalmas_crowdsale_address` variable.
         Dalmas_crowdsale_address = address(DalmasToken_crowdsale);
 
-        // Set the `DalmasTokenCrowdsale` contract as a minter
+        // Set the `DalmasCoinCrowdsale` contract as a minter
         token.addMinter(Dalmas_crowdsale_address);
+        token.addMinter(Dalmas_token_address);
+        token.addMinter(msg.sender);
+        // Have the `DalmasCoinCrowdsaleDeployer` renounce its minter role.
+        //token.renounceMinter();
         
-        // Have the `DalmasTokenCrowdsaleDeployer` renounce its minter role.
-        token.renounceMinter();
     }
 }
